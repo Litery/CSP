0af2d28d33343994c824eafcffa059de72cce199
Szon <s.litera@gmail.com>
2017-05-09 01:13:09 +0200

    Separate abstracts from the implementation

--- CSP.py
+++ CSP.py
@@ -1,54 +1,8 @@
-from abc import ABC, abstractmethod
-from typing import List, Tuple, TypeVar, Iterable
+from typing import List, Tuple
 import numpy
 import time
 import random
-
-
-Move = TypeVar('Move')
-Node = TypeVar('Node')
-
-
-class Problem(ABC):
-    @abstractmethod
-    def print(self) -> None:
-        pass
-
-    @abstractmethod
-    def is_done(self) -> bool:
-        pass
-
-    @abstractmethod
-    def get_next_node(self) -> Node:
-        pass
-
-    @abstractmethod
-    def get_next_nodes(self, length: int) -> List[Node]:
-        pass
-
-    @abstractmethod
-    def load_next_nodes(self, node) -> None:
-        pass
-
-    @abstractmethod
-    def get_moves(self, node: Node) -> Iterable[Move]:
-        pass
-
-    @abstractmethod
-    def has_moves(self, node: Node) -> bool:
-        pass
-
-    @abstractmethod
-    def move(self, move: Move, node: Node):
-        pass
-
-    @abstractmethod
-    def back(self, move: Move, node: Node):
-        pass
-
-    @abstractmethod
-    def rate_move(self, move: Move):
-        pass
+from abstracts import Problem
 
 
 class CSP:
--- abstracts.py
+++ abstracts.py
@@ -1,35 +1,49 @@
 from abc import ABC, abstractmethod
-from typing import List
-from typing import NewType
+from typing import List, Iterable, TypeVar
 
 
-class Move(ABC):
-    pass
 
-
-class State(ABC):
-    @abstractmethod
-    def add_moves(self):
-        pass
-
-    @abstractmethod
-    def move(self, move: Move):
-        pass
+Move = TypeVar('Move')
+Node = TypeVar('Node')
 
 
 class Problem(ABC):
     @abstractmethod
-    def is_done(self):
+    def print(self) -> None:
         pass
 
     @abstractmethod
-    def get_moves(self, moves: List[Move]):
+    def is_done(self) -> bool:
         pass
 
     @abstractmethod
-    def move(self, move: Move):
+    def get_next_node(self) -> Node:
         pass
 
     @abstractmethod
-    def back(self, move: Move):
+    def get_next_nodes(self, length: int) -> List[Node]:
+        pass
+
+    @abstractmethod
+    def load_next_nodes(self, node) -> None:
+        pass
+
+    @abstractmethod
+    def get_moves(self, node: Node) -> Iterable[Move]:
+        pass
+
+    @abstractmethod
+    def has_moves(self, node: Node) -> bool:
+        pass
+
+    @abstractmethod
+    def move(self, move: Move, node: Node):
+        pass
+
+    @abstractmethod
+    def back(self, move: Move, node: Node):
+        pass
+
+    @abstractmethod
+    def rate_move(self, move: Move):
         pass
