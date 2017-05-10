055212fdca9845cad4cf549b0ee60bbec964d7cb
Szon <s.litera@gmail.com>
2017-05-10 02:46:09 +0200

    Parametrize some scripts.

--- Runner.py
+++ Runner.py
@@ -4,14 +4,19 @@ from Queens import Queens
 from CSP import CSP
 
 
-def test1(traverse, insight, scope):
+def test1(traverse, insight, scope, init=None):
     def current_milli_time(): return int(round(time.time() * 1000))
     cols = ['Size', 'BT_time', 'FC_time', 'BT_iters', 'FC_iters']
     row_format = "{:>15}" * 5
     print(row_format.format(*cols))
     for i in range(*scope):
+        if i in [8]:
+            continue
+        if insight is None:
+            insight = i
         data = []
-        init = (int(i / 2), int(i / 2))
+        if init is None:
+            init = (int(i / 2), int(i / 2))
         data.append(i)
         problem1 = Crate(i, init)
         problem2 = Crate(i, init)
@@ -22,7 +27,7 @@ def test1(traverse, insight, scope):
         data.append(timer)
         csp2 = CSP(problem2)
         timer = current_milli_time()
-        csp2.forward_checking(max_insight=i, traverse=traverse)
+        csp2.forward_checking(max_insight=insight, traverse=traverse)
         timer = current_milli_time() - timer
         data.append(timer)
         data.append(csp1.visits)
@@ -48,14 +53,15 @@ def test4(size):
     csp = CSP(problem)
     csp.forward_checking(-1, True, 0)
 
-def test5(scope):
+def test5(scope, init = None):
     def current_milli_time(): return int(round(time.time() * 1000))
     cols = ['Size', 'BT_time', 'FC_time', 'BT_iters', 'FC_iters']
     row_format = "{:>15}" * 5
     print(row_format.format(*cols))
     for i in range(*scope):
         data = []
-        init = (int(i / 2), int(i / 2))
+        if init is None:
+            init = (int(i / 2), int(i / 2))
         data.append(i)
         problem1 = Queens(i, init)
         problem2 = Queens(i, init)
@@ -73,5 +79,12 @@ def test5(scope):
         data.append(csp2.visits)
         print(row_format.format(*data))
 
-# test1(2, 12, (2, 9))
-test5((5, 15))
+
+test1(3, None, (3, 13, 2))
+test1(3, 3, (3, 13, 2))
+test1(3, 6, (3, 13, 2))
+test1(3, 9, (3, 13, 2))
+# test1(3, 12, (3, 15, 2), (0, 0))
+# test5((6, 15, 2))
+# test5((6, 15, 2), (0, 0))
+# test5((5, 15))
